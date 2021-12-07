bfb7a14e507fcdc9103ca54d55ad75001287452c
nazdak504 <aguiluz.matt@gmail.com>
2021-12-06 22:22:17 -0600

    initial dish index fetch completed and tested

--- neon-pyramid/src/App.js
+++ neon-pyramid/src/App.js
@@ -1,29 +1,82 @@
 import './App.css';
-import React from 'react';
-import LoginRegister from './components/LoginRegister';
+import React, { Component } from 'react';
+// import LoginRegister from './components/LoginRegister';
 import Logo from './components/Logo';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
-import Dish from './components/Dish';
+// import Dish from './components/Dish';
 import Order from './components/Order';
-import Payment from './components/Payment';
+// import Payment from './components/Payment';
 
 
-function App() {
-  return (
-    <div className="App">
+let baseUrl = process.env.REACT_APP_BASEURL
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      phone_num: '',
+      address: '',
+      payment_info: {},
+      userLoggedIn: false,
+      modalOpen: false,
+      orders: [],
+      orderToBeEdited: {},
+      dishes: [],
+    }
+  }
+
+  getDishes = () => {
+    // console.log('hit getDishes')
+    fetch(baseUrl + '/dishes/', {
+      credentials: 'include'
+    })
+    .then (res => {
+      if(res.status === 200) {
+        console.log('🥡 fetch index dishes successful! 🥡')
+        return res.json()
+      } else {
+        console.log('fetch index dishes failed...💣')
+        return[]
+      }
+    })
+    .then(data => {
+      console.log('data: ' + data)
+      this.setState({
+        dishes: data.data
+      })
+      console.log('new data: ' + data)
+    })
+  } 
+
+  componentDidMount(){
+    this.getDishes()
+  }
+  
+  render(){
+
+    // this.getDishes()
+
+    return (
+      <div className="App">
       <Logo />
       <Nav />
 
       <div className="bottom-container">
-        <Menu />
+        <Menu 
+          dishes = {this.state.dishes}
+        />
         <Order />
       </div>
       {/* <LoginRegister /> */}
       {/* <Dish /> */}
       {/* <Payment /> */}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- neon-pyramid/src/components/Menu.js
+++ neon-pyramid/src/components/Menu.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function Menu(props) {
+    // console.log('props.dishes[0]: '+ props.dishes[0].title)
     return (
         <div className="component menu">
             <h1>MENU</h1>
             <div className="menu-items">
                 <h3>DRINKS</h3>
+                {props.dishes[0].title}
                 <h3>APPS</h3>
                 <h3>ENTREÉS</h3>
                 <h3>DESSERTS</h3>
